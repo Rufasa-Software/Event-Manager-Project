c8b5a119ae2ed58b54e936615dc250042f70f48e
Alba A.C <albazcano@gmail.com>
2022-01-28 02:10:41 +0100

    update

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,40 +1,27 @@
 <div>
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-        <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title">Tech Event Manager</h5>
-                  {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> --}}
-                </div>
-                <div class="modal-body">
-                  <div class="mb-3">
-                     <input id="event_name" name= "event_name" type="text" class="form-control" placeholder="Nombre del Evento" tabindex="1">
-                  </div>
-      
-                  <div class="mb-3">
-                      <input id="event_date" name= "event_date" type="date" class="form-control"  placeholder="Fecha del evento" tabindex="2">
-                   </div>
-      
-                  <div class="mb-3">
-                      <textarea id="event_desciption" name="event_desciption" class="form-control" placeholder= "Descripción del Evento"rows="3" tabindex="3"></textarea>
-                  </div>
-                    
-                  <div class="mb-3">
-                      <label for="formFile" class="form-label">Adjuntar/Cambiar foto</label>
-                      <input id="event_image" name="event_image" class="form-control" type="file" tabindex="4">
-                    </div>
-  
-                    <div class="mb-3">
-                      <input id="event_capacity" name=event_capacity" type="text" class="form-control"  placeholder="Capacidad del Evento"tabindex="4">
-                   </div>
-      
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                  <button type="button" class="btn btn-primary">Guardar</button>
-                </div>
-              </div>
-            </div>
-      </div>
+  <div class="mb-3">
+    <input type="text" class="form-control" id="event_name" placeholder="Nombre del evento">
+  </div>
+
+  <div class="mb-3">
+  <input type="date" class="form-control" id="event_date" placeholder="Fecha del evento">
+  </div>
+
+  <div class="mb-3">
+    <textarea id="event_desciption" name="event_desciption" class="form-control" placeholder= "Descripción del Evento"rows="3" tabindex="3"></textarea>
+  </div>
+
+  <div class="mb-3">
+    <label for="formFile" class="form-label"></label>
+    <input id="event_image" name="event_image" class="form-control" type="file" tabindex="4">
+  </div>
+
+  <div class="mb-3">
+    <input id="event_capacity" name=event_capacity" type="text" class="form-control"  placeholder="Capacidad del Evento"tabindex="4">
+  </div>
+
+  <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><a id="link_admin" href="/admin">Cerrar</a></button>
+    <button type="button" class="btn btn-primary"><a id="link_admin" href="/admin">Guardar</a></button>
+  </div>
 </div>
--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -8,32 +8,6 @@
     <link rel="stylesheet" href="../css/app.css">
 </head>
 <body>
-
-    <div class="mb-3">
-        <input type="text" class="form-control" id="event_name" placeholder="Nombre del evento">
-    </div>
-
-    <div class="mb-3">
-       <input type="date" class="form-control" id="event_date" placeholder="Fecha del evento">
-    </div>
-
-    <div class="mb-3">
-        <textarea id="event_desciption" name="event_desciption" class="form-control" placeholder= "Descripción del Evento"rows="3" tabindex="3"></textarea>
-    </div>
-
-    <div class="mb-3">
-        <label for="formFile" class="form-label"></label>
-        <input id="event_image" name="event_image" class="form-control" type="file" tabindex="4">
-    </div>
-
-    <div class="mb-3">
-        <input id="event_capacity" name=event_capacity" type="text" class="form-control"  placeholder="Capacidad del Evento"tabindex="4">
-    </div>
-
-    <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><a id="link_admin" href="/admin">Cerrar</a></button>
-        <button type="button" class="btn btn-primary"><a id="link_admin" href="/admin">Guardar</a></button>
-      </div>
-
+    <x-modal />    
 </body>
 </html>
