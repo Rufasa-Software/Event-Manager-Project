5e8e4fd624d48d297d3dc920314f381f286dc568
irina-p-d <irinapetrovap@gmail.com>
2022-02-01 10:14:22 +0100

    commit before merge

--- resources/views/components/tabs-component.blade.php
+++ resources/views/components/tabs-component.blade.php
@@ -13,77 +13,12 @@
           </div>
     </div>
 </section>
-<!---- Card pasar por componente card. Esta aqui para hacer pruebas y comprobar apariencia visual-->
 
-{{-- <section id="section-cards">
+<section id="section-cards">
   
-  <div class="card mb-3 card-event">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img src="" class="img-fluid rounded-start" alt="Image of event">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="date-event">Date event</p>
-          <button type="button" class="btn btn-primary btn-lg">Apuntarse</button>
-        </div>
-      </div>
-    </div>
-    <div class="collapse-container">
-      <p class="collapse-description">
-        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">
-          + info &nbsp &nbsp<i class="fas fa-chevron-down"></i>
-        </button>
-      </p>
-      <div class="row">
-        <div class="col">
-          <div class="collapse multi-collapse" id="multiCollapseExample1">
-            <div class="card card-body card-description">
-              Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
-              <p>Plazas disponibles: 10</p>
-            </div>
-            
-          </div>
-        </div>
-      </div>
-    </div>
-</div>
+ 
 
 
-<div class="card mb-3 card-event">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="" class="img-fluid rounded-start" alt="Image of event">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="date-event">Date event</p>
-        <button type="button" class="btn btn-primary btn-lg">Apuntarse</button>
-      </div>
-    </div>
-  </div>
-  <div class="collapse-container">
-    <p class="collapse-description">
-      <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">
-        + info &nbsp &nbsp<i class="fas fa-chevron-down"></i>
-      </button>
-    </p>
-    <div class="row">
-      <div class="col">
-        <div class="collapse multi-collapse" id="multiCollapseExample2">
-          <div class="card card-body card-description">
-          Aqui tenemos descripcióndel evento.
-            <p>Plazas disponibles: 15</p>
-          </div>
-        </div>  
-      </div>
-    </div>
-  </div>
-</div>
-    
-   --}}
 </section>
 
 
