df068f4b28eb77dd7059db5c5c9e8645aad03c77
Albazc <albazcano@gmail.com>
2022-02-12 19:31:59 +0100

    modified carousel.blade.php and deleted carousel.scss

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,39 +1,23 @@
-<div class="container-fluid mb-3">
-
+<div class="container-fluid mb-4">
   <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
 
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://loremflickr.com/320/240/computer" class="mx-auto d-block w-50">
       </div>
-
-      <div class="carousel-inner">
-        @foreach ($nextEvents as $event)
-
-          <div class="carousel-item active d-flex justify-content-center">
-            <img src="{{$event->event_image(0)}}" class="d-block w-50">
-          </div>
-
-          <div class="carousel-item active d-flex justify-content-center">
-            <img src="{{$event->event_image(1)}}" class="d-block w-50">
-          </div>
-
-          <div class="carousel-item active d-flex justify-content-center">
-            <img src="{{$event->event_image(2)}}" class="d-block w-50">
-          </div>
-          
-        @endforeach
-        
+      <div class="carousel-item">
+        <img src="https://loremflickr.com/320/240/mobilephone" class="mx-auto d-block w-50">
       </div>
+      <div class="carousel-item">
+        <img src="https://loremflickr.com/320/240/laptop" class=" mx-auto d-block w-50">
+      </div>
+    </div>
 
-   {{--  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button> --}}
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    
   </div>
 </div>
